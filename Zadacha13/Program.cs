890336d99620bb2c310c86fc1422435ce7b7c670
VladimirPigarev <109297010+VladimirPigarev@users.noreply.github.com>
2022-08-10 23:17:02 +0300

    Попытка написать код для решения задачи № 13

---
+++ Zadacha13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача № 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите число");
+int number = Convert.ToInt32(ReadLine());
+if(number > 99)
+{
+    int arraynumber = number[];
+}
+else
+{
+    WriteLine("Третьей цифры не существует");
+}
