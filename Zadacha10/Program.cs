03df61781556e931faa82839d3329bdf2fd6a814
VladimirPigarev <109297010+VladimirPigarev@users.noreply.github.com>
2022-08-10 21:37:09 +0300

    Написал код для решения задачи № 10

---
+++ Zadacha10/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача № 10. Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа.
+
+using static System.Console;
+Clear();
+
+int DifinOfTheSecondNumber(int number1) // функция определения второго числа
+{
+    int secondnumber = (number1 % 100) / 10;
+    return secondnumber;
+}
+
+WriteLine("Введите трехзначное чиcло");
+int number = Convert.ToInt32(ReadLine());
+if(number > 99 && number < 1000)
+{
+    int secondnumberresult = DifinOfTheSecondNumber(number);
+    WriteLine(secondnumberresult);
+}
+else
+{
+    WriteLine("Введенное число не является трехзначным");
+}
