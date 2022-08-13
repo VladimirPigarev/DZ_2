424dacb1f77f388ffb6600136b78f2d6563a5a9b
VladimirPigarev <109297010+VladimirPigarev@users.noreply.github.com>
2022-08-13 21:28:07 +0300

    Второй вариант решения задачи

---
+++ Zadacha13_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача № 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+using static System.Console;
+Clear();
+WriteLine("Введите пятизначное число: ");
+int n = Convert.ToInt32(ReadLine());
+if(n > 99 && n < 100000)
+{
+    int result = (n % 1000) / 100;
+    WriteLine("Третья цифра = " + (result));
+}
+else
+{
+    WriteLine("Третьей цифры нет");
+}
